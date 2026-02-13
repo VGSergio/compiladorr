e7081368ad8b9f8cb97c494117b198f2c577bc4a
VGSergio <svegasise@gmail.com>
2026-02-13 22:17:21 +0100

    fix

---
+++ src/main/java/com/vgs/compilador/Main.java
@@ -0,0 +1,12 @@
+package com.vgs.compilador;
+
+/**
+ *
+ * @author sergi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
